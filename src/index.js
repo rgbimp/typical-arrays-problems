e1de3d5beb18e38b1e62ca04b300fc3838cde644
AlenaKuznetsova <rgb.imp@gmail.com>
2021-03-10 16:07:36 +0300

    feat: add realization functions min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length) ? array.sort( (a, b) => a-b )[0]: 0; 
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length ? array.sort( (a, b) => a-b )[array.length - 1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array && array.length ? array.reduce((accum, item) => accum + item) / array.length : 0;
 }
